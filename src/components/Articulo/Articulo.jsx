0e15b2c4a7a1538d12ecdcb702e417f38cf49e29
GuillermoSM <guillermosotomarquez5@gmail.com>
2021-04-26 17:41:03 -0500

    se soluciona problema con el useEffect

--- src/components/Articulo/Articulo.jsx
+++ src/components/Articulo/Articulo.jsx
@@ -1,40 +1,36 @@
-import React,{useState,useEffect} from 'react';
-import {useParams} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import Style from 'styled-components';
-import Error404 from '../Error404/Error404'
+import Error404 from '../Error404/Error404';
 
 const Articulo = () => {
-    const {name} = useParams();
-    const [pokemon,setPokemon]=useState({});
-    const [error,setError]=useState(false);
+	const { name } = useParams();
+	const [pokemon, setPokemon] = useState({});
+	const [error, setError] = useState(false);
 
-    useEffect(()=>{
-        fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-        .then(res=>{
-              if(res.ok){
-                 setError(false)
-                 return res.json()
-                 .then(data=>setPokemon(data.sprites))
-                }
-              else setError(true);
-            })
-      },[])
+	useEffect(() => {
+		fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then((res) => {
+			if (res.ok) {
+				setError(false);
+				return res.json().then((data) => setPokemon(data.sprites));
+			} else setError(true);
+		});
+	}, [name]);
 
-    return (  
-     <ContenedorPokemon>
-       {error && <Error404/>}
-       <img src={pokemon.back_default} alt=""/>
-       <img src={pokemon.front_default} alt=""/>
-       <img src={pokemon.front_shiny} alt=""/>
-       <img src={pokemon.back_shiny} alt=""/>
-     </ContenedorPokemon>    
-          
-    );
-}
+	return (
+		<ContenedorPokemon>
+			{error && <Error404 />}
+			<img src={pokemon.back_default} alt='' />
+			<img src={pokemon.front_default} alt='' />
+			<img src={pokemon.front_shiny} alt='' />
+			<img src={pokemon.back_shiny} alt='' />
+		</ContenedorPokemon>
+	);
+};
 
 const ContenedorPokemon = Style.div`
     display: flex;
     justify-content: space-around;
 `;
- 
+
 export default Articulo;
